70b3d509ecc7deef21cc038b226840dfb8b6cc5e
Pedro Marques <pevimarques@gmail.com>
2023-04-25 00:58:27 -0300

    Arrays-java

---
+++ numerosAleatorios.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+public class numerosAleatorios {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for(int i = 0; i <  numerosAleatorios.length; i++){
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+
+        }
+
+        System.out.println("Numeros Aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println(numero + " ");
+        }
+
+        System.out.println("\nSucessores de Numeros Aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println((numero+1) + " ");
+        }
+        System.out.println("\nAntecessor de Numeros Aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println((numero-1) + " ");
+        }
+      
+    }
+}
